c6afb8ae4f59de3b345bd00a24b7b151c4aaa8e7
Ameer Hamza <ameer.hamza0046@gmail.com>
2023-06-22 14:57:13 +0530

    completed project

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
 const boxes = document.querySelectorAll(".box");
-const showheight = 0.8*window.innerHeight;
-
+const showheight = 0.8 * window.innerHeight;
 
 const check = () => {
-    boxes.forEach(box => {
-        if(box.getBoundingClientRect().top <= showheight) {
-            box.classList.add("show");
-        }
-        else {
-            box.classList.remove("show");
-        }
-    })
+  boxes.forEach((box) => {
+    if (box.getBoundingClientRect().top <= showheight) {
+      box.classList.add("show");
+    } else {
+      box.classList.remove("show");
+    }
+  });
 };
-
 
 window.addEventListener("scroll", check);
 
