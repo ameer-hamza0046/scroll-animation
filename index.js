d261f739a8060b6436323f7ca9a5244ef195aa2f
Ameer Hamza <ameer.hamza0046@gmail.com>
2023-06-22 14:48:28 +0530

    completed except for styling

---
+++ index.js
@@ -0,0 +1,19 @@
+const boxes = document.querySelectorAll(".box");
+const showheight = 0.8*window.innerHeight;
+
+
+const check = () => {
+    boxes.forEach(box => {
+        if(box.getBoundingClientRect().top <= showheight) {
+            box.classList.add("show");
+        }
+        else {
+            box.classList.remove("show");
+        }
+    })
+};
+
+
+window.addEventListener("scroll", check);
+
+check();
